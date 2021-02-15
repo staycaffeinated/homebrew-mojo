7a148000cc88750b385b3707337353499674e533
Jon Caulfield <danesub@yahoo.com>
2021-02-15 12:09:41 -0600

    Initial code base

---
+++ Formula/mojo.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class MojoApplicationShadow < Formula
+  desc "A code generator of Spring-based REST services"
+  homepage ""
+  url "file:///${HOME}/workspace/homebrew-mojo/mojo-application-shadow-0.1.4.tar.gz"
+  sha256 "96e158c92acae6ff6f4998f9b764dd9d1c8df46a72baefbcc329b37b1bef3451"
+  license "Apache 2.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test mojo-application-shadow`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
---
+++ README.md
@@ -0,0 +1,2 @@
+This is the homebrew tap for the staycaffeinated/Mojo2 code generator.
+
