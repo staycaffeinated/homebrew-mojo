85673d4587b9cdfc119797fa9448446738051252
Jon Caulfield <danesub@yahoo.com>
2021-03-29 09:14:49 -0500

    publishing version 0.1.7

--- Formula/mojo.rb
+++ Formula/mojo.rb
@@ -3,8 +3,8 @@
 class Mojo < Formula
   desc "A code generator for Spring-based REST services"
   homepage "https://github.com/staycaffeinated/homebrew-mojo/"
-  url "https://raw.githubusercontent.com/staycaffeinated/homebrew-mojo/main/mojo-application-0.1.6.tar.gz"
-  sha256 "0b0a725de967374512a9d199638d22a8afeddc9bdfab4033ca2be72d35b75042"
+  url "https://raw.githubusercontent.com/staycaffeinated/homebrew-mojo/main/mojo-application-0.1.7.tar.gz"
+  sha256 "2e243e16425283629c6c25617a481192739d4e32da62ba320ea29e220872bd12 "
   license "Apache 2.0"
   # Compilation is not needed
   bottle :unneeded
@@ -13,10 +13,10 @@ class Mojo < Formula
     # put the extracted jar into the "private" libexec folder
     # homebrew automatically unarchives the tar.gz file for us,
     # leaving us with the uber jar that gets copied to the install directory
-    libexec.install "mojo-application-0.1.6-all.jar"
+    libexec.install "mojo-application-0.1.7-all.jar"
     # create a shell script to launch the jar file in the "public" bin folder
     # The shell script will have the entry 'java -jar mojo-application-x.y.z-all.jar'
-    bin.write_jar_script libexec/"mojo-application-0.1.6-all.jar", "mojo"
+    bin.write_jar_script libexec/"mojo-application-0.1.7-all.jar", "mojo"
   end
 
   test do
